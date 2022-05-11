d2885bc074098bfceb1cb2d241536f8e8e558ea7
Webdgepl <cholewski185@gmail.com>
2022-05-11 21:17:42 +0200

    Add frontpage banners

---
+++ wp-content/themes/ifbbpro/template-parts/frontpage-banners.php
@@ -0,0 +1,60 @@
+<section class="frontpageBanners">
+
+
+    <?php $lang = pll_current_language(); ?>
+
+    <a href="https://fiwe.pl/" target="_blank">
+        <figure class="fullWidthBanner">
+            <img class="fullWidthBanner__img" src="<?php echo get_template_directory_uri() . '/img/frontpage/banners/fiwe__banner.png' ?>" alt="Fiwe">
+        </figure>
+    </a>
+
+    <div class="frontpageBanners__slider">
+
+        <a href="https://www.panattasport.pl" target="_blank">
+            <figure class="fullWidthBanner">
+                <img class="fullWidthBanner__img" src="<?php echo get_template_directory_uri() . '/img/frontpage/banners/panatta_1_' .  $lang . '.png' ?>" alt="Panatta">
+            </figure>
+        </a>
+
+        <a href="https://xfg.pl" target="_blank">
+            <figure class="fullWidthBanner">
+                <img class="fullWidthBanner__img" src="<?php echo get_template_directory_uri() . '/img/frontpage/banners/xtreme_1_' .  $lang . '.png' ?>" alt="Xtreme Fitness Gyms">
+            </figure>
+        </a>
+
+    </div>
+
+    <a href="https://www.fanutrition.pl/<?php echo $lang ?>" target="_blank">
+        <figure class="fullWidthBanner">
+            <img class="fullWidthBanner__img" src="<?php echo get_template_directory_uri() . '/img/frontpage/banners/FA_1_' .  $lang . '.jpg' ?>" alt="Fitness Authority">
+        </figure>
+    </a>
+
+
+
+    <div class="frontpageBanners__slider">
+
+        <a href="https://www.panattasport.pl" target="_blank">
+            <figure class="fullWidthBanner">
+                <img class="fullWidthBanner__img" src="<?php echo get_template_directory_uri() . '/img/frontpage/banners/panatta_2_' .  $lang . '.png' ?>" alt="Panatta">
+            </figure>
+        </a>
+
+        <a href="https://xfg.pl" target="_blank">
+            <figure class="fullWidthBanner">
+                <img class="fullWidthBanner__img" src="<?php echo get_template_directory_uri() . '/img/frontpage/banners/xtreme_2_' .  $lang . '.png' ?>" alt="Xtreme Fitness Gyms">
+            </figure>
+        </a>
+
+    </div>
+
+
+
+    <a href="https://sklepzawodnika.com.pl/" target="_blank">
+        <figure class="fullWidthBanner">
+            <img class="fullWidthBanner__img" src="<?php echo get_template_directory_uri() . '/img/frontpage/banners/protan__banner.jpg' ?>" alt="Protan">
+        </figure>
+    </a>
+
+</section>
