5192560dc553f4b10abfafa7725c3ff1f4c71c87
Webdgepl <cholewski185@gmail.com>
2022-05-11 21:22:57 +0200

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,12 @@ const autoprefixer = require("gulp-autoprefixer");
 var uglifycss = require("gulp-uglifycss");
 var sourcemaps = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
-var copy = require("gulp-copy");
 var plumber = require("gulp-plumber");
 const minify = require("gulp-minify");
 var concat = require("gulp-concat");
 var gulp = require("gulp");
 
-const themeFolder = "wp-content/themes/ifbbpro"; ////Change this while changing project
+const themeFolder = "wp-content/themes/ifbbpro";
 
 gulp.task("compile-scss", function () {
 	return gulp
@@ -26,15 +25,12 @@ gulp.task("compile-scss", function () {
 });
 
 gulp.task("compile-js", function () {
-	//gulp.src([themeFolder + '/js/*.js'])
-
 	return gulp
 		.src([
 			themeFolder + "/js/flipclock.min.js",
 			themeFolder + "/js/font-awesome.js",
 			themeFolder + "/js/slick.js",
 			themeFolder + "/js/main.js",
-			themeFolder + "/js/react.js",
 		])
 		.pipe(concat("main.js"))
 		.pipe(
