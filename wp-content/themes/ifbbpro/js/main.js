ed15ce256e5b27f089de36ca58eb44bc2808a8cc
Webdgepl <cholewski185@gmail.com>
2022-06-01 10:49:00 +0200

    Update countdown for next Festival

--- wp-content/themes/ifbbpro/js/main.js
+++ wp-content/themes/ifbbpro/js/main.js
@@ -154,7 +154,7 @@ const countdown = () => {
 		var currentDate = new Date();
 
 		// Set some date in the future
-		var futureDate = new Date(2022, 4, 28, 10, 0, 0);
+		var futureDate = new Date(2022, 9, 10, 10, 0, 0);
 
 		// Calculate the difference in seconds between the future and current date
 		var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
