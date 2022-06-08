01eb01289a51c387e91c4be0f71908f25a7afed4
Webdgepl <cholewski185@gmail.com>
2022-06-09 00:03:47 +0200

    Change order of logos in frontpage

--- wp-content/themes/ifbbpro/template-parts/sponsors-logotypes.php
+++ wp-content/themes/ifbbpro/template-parts/sponsors-logotypes.php
@@ -8,8 +8,8 @@
         <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/relacja-live/FA.png' ?>" alt="FA">
         <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/partnerzy/panatta.png' ?>" alt="panatta">
         <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/partnerzy/biowellpolska.png' ?>" alt="Biowell">
+        <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/partnerzy/ptak1.png' ?>" alt="Ptak">
         <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/relacja-live/Sklepzawodnika.png' ?>" alt="Sklep zawodnika">
-        <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/partnerzy/ptak.png' ?>" alt="Bad Iron">
         <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/partnerzy/mcer.png' ?>" alt="Mcer">
         <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/partnerzy/fiwe-color2.png' ?>" alt="Fiwe">
         <img class="sponsorsBar__img" src="<?php echo get_template_directory_uri() . '/img/partnerzy/13.png' ?>" alt="PRO TAN">
