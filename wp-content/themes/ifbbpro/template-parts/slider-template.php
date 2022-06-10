8d812a2a2ebeb1ce3ba3342055bd82f49ea71300
Webdgepl <cholewski185@gmail.com>
2022-06-11 00:56:14 +0200

    Add Ptak Expo in sponsors slider

--- wp-content/themes/ifbbpro/template-parts/slider-template.php
+++ wp-content/themes/ifbbpro/template-parts/slider-template.php
@@ -49,6 +49,10 @@
     <div><img src="<?php bloginfo('template_directory'); ?>/img/partnerzy/fiwe-color2.png" alt="sponsor-logo"></div>
 </a>
 
+<a href="https://warsawexpo.eu/" target="_blank">
+    <div><img src="<?php bloginfo('template_directory'); ?>/img/partnerzy/ptak1.png" alt="sponsor-logo"></div>
+</a>
+
 
 <a href="https://www.facebook.com/streetmeat.truck/" target="_blank">
     <div><img src="<?php bloginfo('template_directory'); ?>/img/logon1.jpg" alt="sponsor-logo"></div>
