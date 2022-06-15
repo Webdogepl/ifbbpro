d0d65b53d75895f51d5dc7d1121e9ef9e3b9be34
Webdgepl <cholewski185@gmail.com>
2022-06-16 00:44:24 +0200

    Content changes in pro show shop

--- wp-content/themes/ifbbpro/page-pro-show-sklep.php
+++ wp-content/themes/ifbbpro/page-pro-show-sklep.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template name: Pro Show shop 
  */
@@ -14,17 +15,12 @@ get_header(); ?>
 
     <div id="slide_down" class="page-container">
 
-        <img src="/wp-content/themes/ifbbpro/img/kalendarz/new_3.jpg" class="gutenberg-image" alt="Banner">
-        <img src="/wp-content/themes/ifbbpro/img/kalendarz/new_5.png" class="gutenberg-image" alt="Banner">
-        <img src="/wp-content/themes/ifbbpro/img/kalendarz/new_8.jpg" class="gutenberg-image" alt="Banner">
-
         <div class="cognito">
             <script src="https://www.cognitoforms.com/s/hLWZOxiSs0uV_r9wMdO40A"></script>
             <script>
                 Cognito.load("forms", {
                     id: "36"
                 });
-
             </script>
         </div>
 
