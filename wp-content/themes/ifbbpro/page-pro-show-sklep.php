3b81083d71171cdbb5f4798ac7816cf3fcee4632
Webdgepl <cholewski185@gmail.com>
2022-06-30 23:08:46 +0200

    Change pro show shop img

--- wp-content/themes/ifbbpro/page-pro-show-sklep.php
+++ wp-content/themes/ifbbpro/page-pro-show-sklep.php
@@ -16,12 +16,11 @@ get_header(); ?>
     <div id="slide_down" class="page-container">
 
         <div class="cognito">
-            <script src="https://www.cognitoforms.com/s/hLWZOxiSs0uV_r9wMdO40A"></script>
-            <script>
-                Cognito.load("forms", {
-                    id: "36"
-                });
-            </script>
+            <div class="top-img fullWidthBanner" style="text-align:center; width:100%;"><img style="width:100%" class="mobile" src="/wp-content/themes/ifbbpro/img/frontpage/banners/olympia7.jpg" alt="Rejestracja"></div>
+            <div class="top-img fullWidthBanner" style="text-align:center; width:100%;"><img style="width:100%" class="large" src="/wp-content/themes/ifbbpro/img/frontpage/banners/olympia7.jpg" alt="Rejestracja"></div>
+
+            <iframe src="https://www.cognitoforms.com/f/hLWZOxiSs0uV_r9wMdO40A/36" style="border:0;width:100%;" height="440"></iframe>
+            <script src="https://www.cognitoforms.com/f/iframe.js"></script>
         </div>
 
 
@@ -30,4 +29,4 @@ get_header(); ?>
 
 </main>
 
-<?php get_template_part('template-parts/bottom-template') ?>
+<?php get_template_part('template-parts/bottom-template'); ?>
